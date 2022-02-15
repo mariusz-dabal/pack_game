329c23818e39368c94926531267e05652fe77885
mariusz <mariusz.dabal@paypo.pl>
2022-02-15 19:33:45 +0100

    Players list

--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Player|null find($id, $lockMode = null, $lockVersion = null)
  * @method Player|null findOneBy(array $criteria, array $orderBy = null)
- * @method Player[]    findAll()
  * @method Player[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PlayerRepository extends ServiceEntityRepository
@@ -17,6 +16,11 @@ class PlayerRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Player::class);
+    }
+
+    public function findAll(): array
+    {
+        return $this->findBy([], array('points' => 'DESC'));
     }
 
     // /**
