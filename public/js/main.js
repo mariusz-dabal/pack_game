b4714a1bf842421efe68bd6d1eee81ca89d2b6b7
mariusz <mariusz.dabal@paypo.pl>
2022-03-21 20:24:06 +0100

    Add navbar

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+document.querySelector('#menuButton')
+  .addEventListener('click', () => {
+    const devider = document.querySelector('.dropdown-divider');
+
+    if (devider.classList.contains('show')) {
+      devider.classList.remove('show')
+    } else {
+      devider.classList.add('show')
+    }
+});
+
