99aeb0362bb77f91cd50ad0cb8a31ee24934a09b
uaciej <amacedmace@gmail.com>
2022-06-24 16:55:13 +0200

    NavBar now highlights the active page

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,13 @@ document.querySelector('#menuButton').addEventListener('click', () => {
     }
 });
 
+var url = location.href.split("/");
+var navLinks = document.getElementsByTagName("ul")[0].getElementsByTagName("a");
+var i=0;
+var currentPage = url[url.length - 1];
+for(i;i<navLinks.length;i++){
+  var lb = navLinks[i].href.split("/");
+  if(lb[lb.length-1] == currentPage) {
+   navLinks[i].className = "nav-link active";
+}};
+
